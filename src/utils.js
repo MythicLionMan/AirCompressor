be45ef67e106dffd0daf1c8c1b2f84840c578683
Patrick Rogers <progers@alumni.uwaterloo.ca>
2022-10-29 00:15:42 -0400

    Added a compressor state chart. Right now the chart is a placeholder. I have to implement fetching and displaying compressor data.

--- src/utils.js
+++ src/utils.js
@@ -79,3 +79,68 @@ function fetchState() {
        console.error('Communication Error:', error);
     });
 }
+
+chartMonitorId = null;
+chart = null;
+function monitorChart(chartId, interval = 5000) {
+    if (interval == 0) {
+        if (chartMonitorId) {
+            clearInterval(chartMonitorId);
+            chartMonitorId = null;
+        }
+    } else {
+        if (!chart) {
+            if (document.readyState === 'complete') {
+                chart = configureChart(document.getElementById(chartId).getContext('2d'));
+            } else {
+                console.log('Document is not ready, scheduling chart setup when page is loaded.');
+                window.onload = function() {
+                    chart = configureChart(document.getElementById(chartId).getContext('2d'));
+                }
+            }
+        }
+        chartMonitorId = setInterval(function(){ updateChart() }, interval);
+    }
+}
+
+function configureChart(ctx) {
+    console.log('Document is ready, setting up chart');
+    return new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+function updateChart(chartId) {
+    console.log('Updating chart…');
+}
